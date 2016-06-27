9e84b688e3b4d655b8131edbe8dcb8809223d20b
Nat! <nat@mulle-kybernetik.com>
2016-06-28 00:15:40 +0200

    Simplify NSString for FreeBSD and Linux

--- src/FreeBSD/NSString+FreeBSD.m
+++ src/FreeBSD/NSString+FreeBSD.m
@@ -24,58 +24,33 @@
 
 // should probably query system locale or something
 
-+ (NSStringEncoding) defaultCStringEncoding
-{
-   return( NSUTF8StringEncoding);
-}
-
-
-- (NSStringEncoding) defaultCStringEncoding
-{
-   return( NSUTF8StringEncoding);
-}
-
-
-+ (id) stringWithCString:(char *) s
-{
-   return( [[[self alloc] initWithCString:s] autorelease]);
-}
-
-
-+ (id) stringWithCString:(char *) s
-                  length:(NSUInteger) len
-{
-   return( [[[self alloc] initWithCString:s
-                                   length:len] autorelease]);
-}
-
-
-- (id) initWithCString:(char *) s
-                length:(NSUInteger) len
-{
-   return( [self initWithBytes:s
-                        length:len
-                      encoding:[self defaultCStringEncoding]]);
-}
-
-
-- (id) initWithCString:(char *) s
-{
-   return( [self initWithBytes:s
-                        length:strlen( s)
-                      encoding:[self defaultCStringEncoding]]);
-}
-
-
 - (NSString *) _stringByRemovingPrivatePrefix
 {
    return( self);
 }
 
 
+- (NSUInteger) cStringLength
+{
+   return( [self _UTF8StringLength]);
+}
+
+
 - (char *) cString
 {
    return( (char *) [self UTF8String]);
+}
+
+
+- (NSStringEncoding) _cStringEncoding
+{
+   return( NSUTF8StringEncoding);
+}
+
+
++ (NSStringEncoding) defaultCStringEncoding
+{
+   return( NSUTF8StringEncoding);
 }
 
 @end
