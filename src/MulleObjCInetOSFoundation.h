b7d962aa1c3cc12674ebdad50068d909094d0688
Nat! <nat@mulle-kybernetik.com>
2022-06-23 20:29:20 +0200

    * change GLOBAL for Windows

--- src/MulleObjCInetOSFoundation.h
+++ src/MulleObjCInetOSFoundation.h
@@ -10,7 +10,7 @@
  *
  *  version:  major, minor, patch
  */
-#define MULLE_OBJC_INET_OS_FOUNDATION_VERSION  ((0 << 20) | (20 << 8) | 0)
+#define MULLE_OBJC_INET_OS_FOUNDATION_VERSION  ((0 << 20) | (20 << 8) | 1)
 
 
 static inline unsigned int   MulleObjCInetOSFoundation_get_version_major( void)
@@ -31,7 +31,7 @@ static inline unsigned int   MulleObjCInetOSFoundation_get_version_patch( void)
 }
 
 
-MULLE_OBJC_INET_OS_FOUNDATION_EXTERN_GLOBAL
+MULLE_OBJC_INET_OS_FOUNDATION_GLOBAL
 uint32_t   MulleObjCInetOSFoundation_get_version( void);
 
 
